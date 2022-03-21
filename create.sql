8f74260b5725257e4b3c12606efa559d4b6f718f
natalie <sachua.2020@smu.edu.sg>
2022-03-21 17:30:00 +0800

    updated patient id

--- create.sql
+++ create.sql
@@ -1,36 +1,36 @@
--- create patient database and table
+-- create patient table
 
 drop database if exists patient;
 create database patient;
 use patient;
 
 CREATE TABLE patient (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255),
   age INT NOT NULL,
   allergies VARCHAR(255),
   hp VARCHAR(15) NOT NULL,
   email VARCHAR(255)
 );
 INSERT INTO patient
-  (name,age,allergies,hp,email)
+  (id,name,age,allergies,hp,email)
 VALUES
-  ('Kim Kardashian',41,'Amoxicillin, ampicillin',98443918,'hello@gmail.com'),
-  ('Dwayne Johnson',49,'Aspirin',92345643,'hello@gmail.com'),
-  ('Cardi B',29,'Ibuprofen',91345675,'hello@gmail.com'),
-  ('Ben Affleck',49,'',91345675,'hello@gmail.com'),
-  ('Tom Hanks',65,'',91345675,'hello@gmail.com'),
-  ('Drake',35,'',91345675,'hello@gmail.com'),
-  ('Ellen DeGeneres',64,'',91345675,'hello@gmail.com'),
-  ('Justin Bieber',28,'',91345675,'hello@gmail.com'),
-  ('Adele',30,'',91345675,'hello@gmail.com'),
-  ('Leonardo Dicaprio',34,'',91234567,'hello@gmail.com'),
-  ('George Clooney',45,'Cetuximab',91234567,'hello@gmail.com'),
-  ('Jessica Alba',29,'',91234567,'hello@gmail.com'),
-  ('Angeline Jolie',30,'',91234567,'hello@gmail.com');
+  ('t1234567a','Kim Kardashian',41,'Amoxicillin, ampicillin',91234567,'hello@gmail.com'),
+  ('t1234568a','Dwayne Johnson',49,'Aspirin',92345643,'hello@gmail.com'),
+  ('t1234569a','Cardi B',29,'Ibuprofen',91345675,'hello@gmail.com'),
+  ('t1234570a','Ben Affleck',49,'',91345675,'hello@gmail.com'),
+  ('t1234571a','Tom Hanks',65,'',91345675,'hello@gmail.com'),
+  ('t1234572a','Drake',35,'',91345675,'hello@gmail.com'),
+  ('t1234573a','Ellen DeGeneres',64,'',91345675,'hello@gmail.com'),
+  ('t1234574a','Justin Bieber',28,'',91345675,'hello@gmail.com'),
+  ('t1234575a','Adele',30,'',91345675,'hello@gmail.com'),
+  ('t1234576a','Leonardo Dicaprio',34,'',91234567,'hello@gmail.com'),
+  ('t1234577a','George Clooney',45,'Cetuximab',91234567,'hello@gmail.com'),
+  ('t1234578a','Jessica Alba',29,'',91234567,'hello@gmail.com'),
+  ('t1234579a','Angeline Jolie',30,'',91234567,'hello@gmail.com');
+
   
-  
--- create prescription database and table
+-- create prescription table
 
 drop database if exists prescription;
 create database prescription;
@@ -39,7 +39,7 @@ use prescription;
 CREATE TABLE prescription (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   doctor_id INT NOT NULL,
-  patient_id INT NOT NULL,
+  patient_id VARCHAR(255) NOT NULL,
   description VARCHAR(65535),
   medicines VARCHAR(65535) NOT NULL,
   status VARCHAR(255) NOT NULL
@@ -47,27 +47,26 @@ CREATE TABLE prescription (
 INSERT INTO prescription
   (doctor_id,patient_id,description,medicines,status)
 VALUES
-  (1,1,'Runny nose, sore throat','{"Cymbalta":1, "Omeprazole":2, "Fentanyl":1}','pending'),
-  (2,2,'Covid','{"Ibuprofen":2}','pending'),
-  (3,3,'Diarrhea','{"Methotrexate":1, "Wellbutrin":1}','confirmed'),
-  (6,4,'Fever','{"Xanax":1, "Azithromycin":1}','pending'),
-  (4,5,'Headache','{"Clonazepam":1}','completed'),
-  (3,6,'Runny nose, sore throat','{"Citalopram":1}','pending'),
-  (2,7,'Flu','{"Imbruvica":2, "Gabapentin":1}','pending'),
-  (5,8,'Dry cough','{"Naloxone":3}','confirmed'),
-  (3,9,'Dizzy','{"Naproxen":2, "Metoprolol":4, "Gabapentin":2}','completed'),
-  (4,10,'Flu','{"Amitriptyline":3}','pending'),
-  (4,11,'Nausea','{"Cyclobenzaprine":2, "Lexapro":1, "Amlodipine":3}','completed'),
-  (2,12,'Fatigue','{"Acetaminophen":2}','completed'),
-  (4,13,'Knee pain','{"Omeprazole":1}','completed'),
-  (2,11,'Dizzy spells, nauseous','{"Januvia":3}','confirmed'),
-  (5,1,'Flu symptoms','{"Entresto":2, "Benzonatate":1, "Hydroxychloroquine":2, "Gabapentin":3}','confirmed'),
-  (5,2,'Food poisoning','{"Methadone":1, "Loratadine":2}','confirmed'),
-  (6,3,'Stomach flu','{"Adderall":4}','pending');
+  (1,'t1234567a','Runny nose, sore throat','{"Cymbalta":1, "Omeprazole":2, "Fentanyl":1}','pending'),
+  (2,'t1234568a','Covid','{"Ibuprofen":2}','pending'),
+  (3,'t1234569a','Diarrhea','{"Methotrexate":1, "Wellbutrin":1}','confirmed'),
+  (6,'t1234570a','Fever','{"Xanax":1, "Azithromycin":1}','pending'),
+  (4,'t1234571a','Headache','{"Clonazepam":1}','completed'),
+  (3,'t1234572a','Runny nose, sore throat','{"Citalopram":1}','pending'),
+  (2,'t1234573a','Flu','{"Imbruvica":2, "Gabapentin":1}','pending'),
+  (5,'t1234574a','Dry cough','{"Naloxone":3}','confirmed'),
+  (3,'t1234575a','Dizzy','{"Naproxen":2, "Metoprolol":4, "Gabapentin":2}','completed'),
+  (4,'t1234576a','Flu','{"Amitriptyline":3}','pending'),
+  (4,'t1234577a','Nausea','{"Cyclobenzaprine":2, "Lexapro":1, "Amlodipine":3}','completed'),
+  (2,'t1234578a','Fatigue','{"Acetaminophen":2}','completed'),
+  (4,'t1234579a','Knee pain','{"Omeprazole":1}','completed'),
+  (2,'t1234577a','Dizzy spells, nauseous','{"Januvia":3}','confirmed'),
+  (5,'t1234567a','Flu symptoms','{"Entresto":2, "Benzonatate":1, "Hydroxychloroquine":2, "Gabapentin":3}','confirmed'),
+  (5,'t1234568a','Food poisoning','{"Methadone":1, "Loratadine":2}','confirmed'),
+  (6,'t1234569a','Stomach flu','{"Adderall":4}','pending');
+
   
-
-
--- create inventory database and table
+-- create inventory table
 
 drop database if exists inventory;
 create database inventory;
@@ -75,8 +74,8 @@ use inventory;
 
 CREATE TABLE inventory (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  price FLOAT NOT NULL,
+  name VARCHAR(255),
+  price FLOAT,
   stock INT
 );
 INSERT INTO inventory
@@ -147,7 +146,7 @@ VALUES
 
 
 
--- create payment database and table
+-- create payment table
 
 drop database if exists payment;
 create database payment;
@@ -156,7 +155,7 @@ use payment;
 CREATE TABLE payment (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   prescription_id INT NOT NULL,
-  patient_id INT NOT NULL,
+  patient_id VARCHAR(255) NOT NULL,
   medicines VARCHAR(65535) NOT NULL,
   total FLOAT NOT NULL,
   order_id VARCHAR(255),
@@ -165,20 +164,20 @@ CREATE TABLE payment (
 INSERT INTO payment
   (prescription_id,patient_id,medicines,total,order_id,status)
 VALUES
-  (1,1,'{"Cymbalta":1, "Omeprazole":2, "Fentanyl":1}',13.12,'dummy','unpaid'),
-  (2,2,'{"Ibuprofen":2}',1.90,'dummy','paid'),
-  (3,3,'{"Methotrexate":1, "Wellbutrin":1}',24.12,'dummy','paid'),
-  (4,4,'{"Xanax":1, "Azithromycin":1}',15.00,'dummy','paid'),
-  (5,5,'{"Clonazepam":1}',7.05,'dummy','paid'),
-  (6,6,'{"Citalopram":1}',3.49,'dummy','paid'),
-  (7,7,'{"Imbruvica":2, "Gabapentin":1}',25.68,'dummy','unpaid'),
-  (8,8,'{"Naloxone":3}',1.32,'dummy','unpaid'),
-  (9,9,'{"Naproxen":2, "Metoprolol":4, "Gabapentin":2}',37.03,NULL,'paid'),
-  (10,10,'{"Amitriptyline":3}',6.18,NULL,'unpaid'),
-  (11,11,'{"Cyclobenzaprine":2, "Lexapro":1, "Amlodipine":3}',17.99,NULL,'unpaid'),
-  (12,12,'{"Acetaminophen":2}',11.56,NULL,'unpaid'),
-  (13,13,'{"Omeprazole":1}',3.71,NULL,'unpaid'),
-  (14,11,'{"Januvia":3}',14.32,NULL,'unpaid'),
-  (15,1,'{"Entresto":2, "Benzonatate":1, "Hydroxychloroquine":2, "Gabapentin":3}',41.66,NULL,'unpaid'),
-  (16,2,'{"Methadone":1, "Loratadine":2}',16.33,NULL,'unpaid'),
-  (17,3,'{"Adderall":4}',0.81,NULL,'unpaid');
+  (1,'t1234567a','{"Cymbalta":1, "Omeprazole":2, "Fentanyl":1}',13.12,'dummy','paid'),
+  (2,'t1234568a','{"Ibuprofen":2}',1.90,'dummy','paid'),
+  (3,'t1234569a','{"Methotrexate":1, "Wellbutrin":1}',24.12,'dummy','paid'),
+  (4,'t1234570a','{"Xanax":1, "Azithromycin":1}',15.00,'dummy','paid'),
+  (5,'t1234571a','{"Clonazepam":1}',7.05,'dummy','paid'),
+  (6,'t1234572a','{"Citalopram":1}',3.49,'dummy','paid'),
+  (7,'t1234573a','{"Imbruvica":2, "Gabapentin":1}',25.68,'dummy','unpaid'),
+  (8,'t1234574a','{"Naloxone":3}',1.32,'dummy','unpaid'),
+  (9,'t1234575a','{"Naproxen":2, "Metoprolol":4, "Gabapentin":2}',37.03,NULL,'paid'),
+  (10,'t1234576a','{"Amitriptyline":3}',6.18,NULL,'unpaid'),
+  (11,'t1234577a','{"Cyclobenzaprine":2, "Lexapro":1, "Amlodipine":3}',17.99,NULL,'unpaid'),
+  (12,'t1234578a','{"Acetaminophen":2}',11.56,NULL,'unpaid'),
+  (13,'t1234579a','{"Omeprazole":1}',3.71,NULL,'unpaid'),
+  (14,'t1234577a','{"Januvia":3}',14.32,NULL,'unpaid'),
+  (15,'t1234567a','{"Entresto":2, "Benzonatate":1, "Hydroxychloroquine":2, "Gabapentin":3}',41.66,NULL,'unpaid'),
+  (16,'t1234568a','{"Methadone":1, "Loratadine":2}',16.33,NULL,'unpaid'),
+  (17,'t1234569a','{"Adderall":4}',0.81,NULL,'unpaid');
